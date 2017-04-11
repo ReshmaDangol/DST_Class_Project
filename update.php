f8cdffcce0bab5dc5e3ae2c1546fe61e36511d01
reshma <resma.dangol@gmail.com>
2017-04-11 12:22:13 +0700

    Add Update Call Log page

---
+++ update.php
@@ -0,0 +1,27 @@
+﻿<?php
+include ("header.html");
+?>
+<div class="col-lg-offset-3 col-lg-6">
+<div class="col-lg-offset-3 col-lg-6">
+ <div class="pageTitle">
+    <h2>
+      <center>
+        Update Call Log <img src="1491831348_thefreeforty_phone.png" width=40 style="margin-bottom:10px">
+      </center>
+    </h2>
+  </div
+<form>
+  <div class="form-group">
+    <label for="pastdate">Date</label>
+    <input type="text" class="form-control" id="pastdate">
+  </div>
+  <div class="form-group">
+    <label for="calls">No of Calls:</label>
+    <input type="number" class="form-control" id="calls">
+  </div>
+ 
+  <center><button type="submit" class="btn btn-primary" id="predict">Save</button>
+  </center>
+</form>
+</div>
+</div>
